7ca80fc584ba7b76f4ad9732f427538adf15778b
Bernardo Alves <bernardoalves330@gmail.com>
2022-04-26 19:28:01 -0300

    commit

--- introducao-web/Portfolio/script.js
+++ introducao-web/Portfolio/script.js
@@ -1,9 +1,9 @@
 function sendEmail(){
     Email.send({
         Host : "smtp.gmail.com",
-        Username : "marquinhofone@gmail.com",
-        Password : "password",
-        To : 'bernardoalves330@gmail.com',
+        Username : "bernardoalves330@gmail.com",
+        Password : "BeRicalves2003",
+        To : 'email.portfolio.web@gmail.com',
         From : document.getElementById('f-email-input').value,
         Subject : "Portfolio site",
         Body : document.getElementById('f-message-area').value
