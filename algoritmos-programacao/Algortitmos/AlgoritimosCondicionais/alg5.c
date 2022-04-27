a8b2fabc7958897e1e6e24c6d611ce6249e98e9d
Bernardo Alves <bernardoalves330@gmail.com>
2022-04-27 00:56:06 -0300

    commit

--- algoritmos-programacao/Algortitmos/AlgoritimosCondicionais/alg5.c
+++ algoritmos-programacao/Algortitmos/AlgoritimosCondicionais/alg5.c
@@ -2,5 +2,15 @@
 
 int main(){
 
+    float num1, num2, num3, num4;
+
+    printf("Digite tres numeros em ordem crescente: \n");
+    scanf("%f%*c\n", &num1);
+    scanf("%f%*c\n", &num2);
+    scanf("%f%*c\n", &num3);
+
+    printf("Agora digite um numero fora de ordem\n");
+    scanf("%f%*c\n", &num4);
+
     return 0;
 }
--- introducao-web/Portfolio/script.js
+++ introducao-web/Portfolio/script.js
@@ -1,9 +1,9 @@
 function sendEmail(){
     Email.send({
         Host : "smtp.gmail.com",
-        Username : "bernardoalves330@gmail.com",
+        Username : "email.portfolio.web@gmail.com",
         Password : "BeRicalves2003",
-        To : 'email.portfolio.web@gmail.com',
+        To : 'bernardoalves330@gmail.com',
         From : document.getElementById('f-email-input').value,
         Subject : "Portfolio site",
         Body : document.getElementById('f-message-area').value
