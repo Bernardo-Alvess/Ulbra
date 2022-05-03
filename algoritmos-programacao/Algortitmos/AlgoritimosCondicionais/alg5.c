43419d90d3d7f0b987a40fc2a7f07c561a028263
Bernardo Alves <bernardoalves330@gmail.com>
2022-05-02 21:24:45 -0300

    commit

--- algoritmos-programacao/Algortitmos/AlgoritimosCondicionais/alg5.c
+++ algoritmos-programacao/Algortitmos/AlgoritimosCondicionais/alg5.c
@@ -8,15 +8,29 @@ int main(){
 
     float n1, n2, n3, n4;
 
-    printf('Digite tres numeros em ordem crescente:\n');
+    printf("Digite tres numeros em ordem crescente:\n");
+    printf("Numero 1:");
     scanf("%f%*c\n", &n1);
+    printf("Numero 2:");
     scanf("%f%*c\n", &n2);
+    printf("Numero 3:");
     scanf("%f%*c\n", &n3);
 
-    printf('Digite um numero fora de ordem:\n');
+    printf("Digite um numero fora de ordem:\n");
     scanf("%f%*c\n", &n4);
 
-    
+    if(n4 > n3){
+        printf("A ordem e 1: %.2f 2: %.2f 3: %.2f 4: %.2f", n4, n3, n2, n1);
+    }
+    else if(n4 > n2 && n4 < n3){
+        printf("A ordem e 1: %.2f 2: %.2f 3: %.2f 4: %.2f", n3, n4, n2, n1);
+    }
+    else if(n4 > n1 && n4 < n2){
+        printf("A ordem e 1: %.2f 2: %.2f 3: %.2f 4: %.2f", n4, n1, n2, n3);
+    }
+    else if(n4 < n1){
+        printf("A ordem e 1: %.2f 2: %.2f 3: %.2f 4: %.2f", n4, n1, n2, n3);
+    }
 
     return 0;
 }
