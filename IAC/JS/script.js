f437d0c2bfe15043dba5c021ef22b9ab07e8a505
Bernardo Alves <bernardoalves330@gmail.com>
2022-05-30 21:46:33 -0300

    arrumando pastas

---
+++ IAC/JS/script.js
@@ -0,0 +1,16 @@
+alert("$UICIDEBOY$");
+
+var music = "all dogs go to heaven"
+
+document.write("<p>FUCK 'EM ALL</p>")
+console.log(`A musica é ${music}`)
+
+
+var teste = 1==1
+var teste2 = 1===1
+var teste3 = 1=="1"
+
+
+console.log(teste)
+console.log(teste2)
+console.log(teste3)
